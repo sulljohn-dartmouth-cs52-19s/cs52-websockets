727d87e1f2dfd631fdb85578b153d945eb1af4b4
John Sullivan <sulljohn12@gmail.com>
2019-05-13 14:47:08 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Websockets Short Assignment
+
+John Sullivan
+
+CS 52
+
+5/13/2019
---
+++ src/server.js
@@ -0,0 +1,101 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
+import socketio from 'socket.io';
+import http from 'http';
+import mongoose from 'mongoose';
+import * as Notes from './controllers/note_controller';
+
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/notes';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
+
+// initialize
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
+
+// enable/disable http request logging
+app.use(morgan('dev'));
+
+// enable only if you want templating
+app.set('view engine', 'ejs');
+
+// enable only if you want static assets from folder static
+app.use(express.static('static'));
+
+// this just allows us to render ejs from the ../app/views directory
+app.set('views', path.join(__dirname, '../src/views'));
+
+// enable json message body for posting data to API
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// additional init stuff should go before hitting the routing
+
+// default index route
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+// START THE SERVER
+// =============================================================================
+const port = process.env.PORT || 9090;
+server.listen(port);
+
+console.log(`listening on: ${port}`);
+
+io.on('connection', (socket) => {
+  // on first connection emit notes
+  Notes.getNotes().then((result) => {
+    socket.emit('notes', result);
+  });
+
+  // pushes notes to everybody
+  const pushNotes = () => {
+    Notes.getNotes().then((result) => {
+      // broadcasts to all sockets including ourselves
+      io.sockets.emit('notes', result);
+    });
+  };
+
+  // creates notes and
+  socket.on('createNote', (fields) => {
+    Notes.createNote(fields).then(() => {
+      pushNotes();
+    }).catch((error) => {
+      console.log(error);
+      socket.emit('error', 'create failed');
+    });
+  });
+
+  // on update note do what is needful
+  socket.on('updateNote', (id, fields) => {
+    Notes.updateNote(id, fields).then(() => {
+      pushNotes();
+    }).catch((error) => {
+      console.log(error);
+      socket.emit('error', 'update failed');
+    });
+  });
+
+  // on deleteNote do what is needful
+  socket.on('deleteNote', (id) => {
+    // console.log(id);
+    Notes.deleteNote(id).then(() => {
+      pushNotes();
+    }).catch((error) => {
+      console.log(error);
+      socket.emit('error', 'delete failed');
+    });
+  });
+});
